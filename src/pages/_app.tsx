7a4b3474517b901b97e48fbee3376ca9e9866075
EduAvelar <eduardo.avelar12@gmail.com>
2021-02-25 18:45:20 -0300

    Dia 4 - NLW
    Mudanças na aplicação, para que o contador volte após clicar em "falhei" e "completei".

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,7 +9,7 @@ function MyApp({ Component, pageProps }) {
     
     <ChallengesProvider>
       <Component {...pageProps} />
-    </ChallengesProvider>    
+    </ChallengesProvider>
         
   )
 }
